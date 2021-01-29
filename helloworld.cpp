89cfde51b69a2a229bc71b8d294c9b0eeb267d7d
djtikky <duangpenj@gmail.com>
2021-01-29 14:32:05 +0700

    commit2 add age
    add age

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,7 +3,11 @@ using namespace std;
 int main()
 {
     string name;
+    int age;
     cout<<"hello world: what is your name";
     cin>>name;
     cout<<"hello "<< name<<endl;
+    cout <<"how old are you";
+    cin>> age;
+    cout <<"you are "<<age<<endl;
 }
